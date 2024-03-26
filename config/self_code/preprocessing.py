7157e00a6bf33b4996a2f94ac898017eeea99886
patrick_Desktop <min2max.dev@gmail.com>
2024-03-26 14:15:44 +0900

    update

--- config/self_code/preprocessing.py
+++ config/self_code/preprocessing.py
@@ -6,29 +6,32 @@ import pandas as pd
 from konlpy.tag import Okt
 
 FILTERS = "([~.,!?\"':;)(])"
-PAD = "<PAD>"
-STD = "<SOS>"
-END = "<EOS>"
-UNK = "<UNK>"
-MARKER =[PAD, STD, END, UNK]
+
 MAX_SEQ = 25
 
 # 데이터 로드 함수
-def load_data(path):
+def load_data(path, file):
     '''
     Args:
         path: String of data file(csv) path
     Returns:
-        qst, ans : Lists of sentences of question ans answer
+        qst, ans: Lists of sentences of question ans answer
     '''
-    df = pd.read_csv(path, encoding='utf-8')
+    df = pd.read_csv(os.path.join(path, file), encoding='utf-8')
     qst = list(df['Q'])
     ans = list(df['A'])
     return qst, ans
 
 # 정규화 함수
-def normalize(sentence, pattern):
-    normalizer = re.compile(pattern)
+def normalize(sentence):
+    '''
+    Args:
+        sentence: String
+    Return:
+        n_sentence: Normalized String
+    '''
+    K_pattern = r'[^ ?,.!A-Za-z0-9가-힣+]'
+    normalizer = re.compile(K_pattern)
     n_sentence = normalizer.sub("", sentence)
     return n_sentence
 
@@ -43,10 +46,28 @@ def morpheming(sentences):
     morpheme_seq = list()
     okt = Okt()
     for sentence in sentences:
-        sentence = re.sub(FILTERS, '', sentence)
+        sentence = normalize(sentence)
         morpheme_seq.extend(okt.morphs(sentence))
     morpheme_seq = list(set(morpheme_seq))
     return morpheme_seq
+
+PAD = 0
+SOS = 1
+EOS = 2
+UNK = 3
+
+word2index = {
+    "<PAD>":PAD,
+    "<SOS>":SOS,
+    "<EOS>":EOS,
+    "<UNK>":UNK
+}
+index2word = {
+    PAD:"<PAD>",
+    SOS:"<SOS>",
+    EOS:"<EOS>",
+    UNK:"<UNK>"
+}
 
 def dictGenerator(path):
     '''
@@ -56,16 +77,22 @@ def dictGenerator(path):
     '''
     qst, ans = load_data(path)
     dictionary = list()
-    dictionary.extend(morpheming(qst))
-    dictionary.extend(morpheming(ans))
+    dictionary.extend(qst)
+    dictionary.extend(ans)
+    dictionary = morpheming(dictionary)
+    dictionary[:0] = MARKER
     return dictionary
 
 def vocabLoader(path, vocab_path):
     '''
+    Args:
+        path: String of data path
+        vocab_path: String of dictionary path
+    Return:
+        words: List of morphemes loaded from vocabulary.txt file
     '''
     if not os.path.exists(vocab_path):
         words = dictGenerator(path)
-        words[:0] = MARKER
         with open(vocab_path, 'w', encoding='utf-8') as vocabulary_file:
             for word in words:
                 vocabulary_file.write(word + '\n')
@@ -80,12 +107,3 @@ def vocabLoader(path, vocab_path):
 def indexing(words):
     words_index = dict([(word, i+1) for i, word in range(enumerate(words))])
     return words_index
-
-def encoding(sentences):
-    '''
-    Args:
-    Returns: Encoded vector(List) made of vocabulary index
-    '''
-    for sentence in sentences:
-        sentence
-    return
--- config/self_code/seq2seq.py
+++ config/self_code/seq2seq.py
@@ -1,20 +1,28 @@
 import torch.nn as nn
 
 class Encoder(nn.Module):
-    def __init__(self, num_vocabs, hidden_size, embedding_dim, num_layers):
-        super(Encoder, self).__init__()
-        
+    def __init__(self, n_vocabs, emb_dim, hid_dim, n_layers, dropout):
+        super().__init__()
+
         # 단어 사전의 개수 지정
-        self.num_vocabs = num_vocabs
+        self.n_vocabs = n_vocabs
+        self.hid_dim = hid_dim
+        self.n_layers = n_layers
+
+        # embedding: n_vocab(input)을 emb_dim vector로 변경
         # 임베딩 레이어 정의 (number of vocabs, embedding dimension)
-        self.embedding = nn.Embedding(num_vocabs, embedding_dim)
-        # GRU (embedding dimension)
-        self.gru = nn.GRU(embedding_dim, 
-                          hidden_size, 
-                          num_layers=num_layers, 
-                          bidirectional=False)
+        self.embedding = nn.Embedding(n_vocabs, emb_dim)
+
+        # GRU (embedding dimension): LSTM에서 사용하는 cell state, hidden state을 하나로 합쳐 hidden state으로 사용
+        self.gru = nn.GRU(emb_dim, hid_dim, n_layers, dropout=dropout, bidirectional=False)
+        
+        self.dropout = nn.Dropout(dropout)
         
     def forward(self, x):
+        # embedding(): input shape, embedding vector의 차원 수를 출력
+        # permute(): 입력된 tensor의 차원 순서를 변경
+        # (1,2,3)으로 구성된 3차원 tensor에 permute(2,1,0)를 적용하면 (3,2,1) 순서로 tensor가 변경
         x = self.embedding(x).permute(1, 0, 2)
+
         output, hidden = self.gru(x)
         return output, hidden
